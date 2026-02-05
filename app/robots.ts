8dd264add09aa008d054444b4c069f232644c71c
Mohammad <itzmohammadkhan@gmail.com>
2026-02-05 08:47:20 +0530

    fix: robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,8 @@
 // app/robots.ts
-export default function robots() {
+
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
